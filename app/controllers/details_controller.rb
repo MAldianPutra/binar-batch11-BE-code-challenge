c00a5b9ef94d56f92316d9a2917bd7ec760a11b3
Aldian Putra <aldianputra99@gmail.com>
2019-03-04 23:09:35 +0700

    Details done

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,13 +1,44 @@
 class DetailsController < ApplicationController
+  before_action :current_detail, only: [:show, :edit, :update, :destroy]
+
   def index
+    @details = Detail.all  
   end
 
   def show
+
   end
 
   def new
+    @detail = Detail.new
+  end
+
+  def create
+    detail = Detail.create(detail_params)
+    redirect_to details_path(detail)
   end
 
   def edit
+
+  end
+
+  def update
+    @detail.update(detail_params)
+    redirect_to details_path(@detail)
+  end
+  
+  def destroy
+    @detail.destroy
+    redirect_to details_path
+  end
+
+  private
+
+  def detail_params
+    params.require(:detail).permit(:item_id, :order_id, :price, :quantity)
+  end
+
+  def current_detail
+    @detail = Detail.find(params[:id])
   end
 end
