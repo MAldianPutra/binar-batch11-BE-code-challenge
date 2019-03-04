a53a68fb3d20c3708ae8c7e85105f31d3b59aa69
Aldian Putra <aldianputra99@gmail.com>
2019-03-04 18:33:51 +0700

    Add order, upcoming store features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :members
   resources :items
+  resources :orders
+  resources :details
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/details_controller_test.rb
+++ test/controllers/details_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class DetailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get details_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get details_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get details_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get details_edit_url
+    assert_response :success
+  end
+
 end
