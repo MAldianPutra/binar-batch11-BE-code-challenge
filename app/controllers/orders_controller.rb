fc14e2be3b67a046a9c9279f6c113c4a633e6e12
Aldian Putra <aldianputra99@gmail.com>
2019-03-04 22:41:40 +0700

    Order done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,44 @@
 class OrdersController < ApplicationController
+  before_action :current_order, only: [:show, :edit, :update, :destroy]
+
   def index
+    @orders = Order.all  
   end
 
   def show
+
   end
 
   def new
+    @order = Order.new
+  end
+
+  def create
+    order = Order.create(order_params)
+    redirect_to orders_path(order)
   end
 
   def edit
+
+  end
+
+  def update
+    @order.update(order_params)
+    redirect_to orders_path(@order)
+  end
+  
+  def destroy
+    @order.destroy
+    redirect_to orders_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:member_id)
+  end
+
+  def current_order
+    @order = Order.find(params[:id])
   end
 end
